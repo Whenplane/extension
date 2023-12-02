cdede99fd2ed9528d2782734e63d240b4c4e8924
ajgeiss0702 <ajgeiss72@gmail.com>
2023-12-01 20:57:34 -0700

    first commit

---
+++ inserter.js
@@ -0,0 +1,21 @@
+
+function replace() {
+
+    const now = new Date();
+
+    // only on friday
+    if(now.getUTCDay() !== 5 && now.getUTCDay() !== 6) return;
+
+    const element = document.querySelector("div.av-player-control-wrapper > div.livestream-offline-container");
+    if(element != null) {
+        element.innerHTML = `<iframe src="https://whenplane.com?frame" style="width:100%;height:100%;">Something went wrong when inserting whenplane frame</iframe>`;
+
+        if(initialInterval) {
+            clearInterval(initialInterval);
+            initialInterval = undefined;
+        }
+    }
+}
+
+// reminder: 500ms breaks the page for some reason
+let initialInterval = setInterval(replace, 1e3);
