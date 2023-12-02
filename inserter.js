e205fcbf0f6c42c599f70f08dfe0105637884513
ajgeiss0702 <ajgeiss72@gmail.com>
2023-12-01 23:37:59 -0700

    only insert if its not already inserted

--- inserter.js
+++ inserter.js
@@ -5,7 +5,9 @@ function replace() {
 
     const element = document.querySelector("div.av-player-control-wrapper > div.livestream-offline-container");
     if(element != null) {
-        element.innerHTML = `<iframe src="https://whenplane.com?frame" style="width:100%;height:100%;">Something went wrong when inserting whenplane frame</iframe>`;
+        if(!element.innerHTML.startsWith("<iframe")) {
+            element.innerHTML = `<iframe src="https://whenplane.com?frame" style="width:100%;height:100%;">Something went wrong when inserting whenplane frame</iframe>`;
+        }
 
         if(initialInterval) {
             clearInterval(initialInterval);
