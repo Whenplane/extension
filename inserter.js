c39f13385b560826c27251c960d42d1dec65c74f
ajgeiss0702 <aj@ajg0702.us>
2024-12-09 12:51:02 -0700

    (wip) add search provider

--- inserter.js
+++ inserter.js
@@ -10,26 +10,29 @@ async function replace() {
     const domain = location.hostname.toLowerCase();
 
     const isFloatplane = domain.includes("floatplane.com");
-    const isTwitch = domain.includes("twitch.tv")
+    const isTwitch = domain.includes("twitch.tv");
+
+    const isBoca = isTwitch && location.pathname.toLowerCase() === "/bocabola_";
 
     // We have to check here instead of in the manifest because the script isnt loaded when navigating client-side
     if(isFloatplane && location.pathname !== "/channel/linustechtips/live") return;
-    if(isTwitch && location.pathname.toLowerCase() !== "/linustech") return;
+    if(isTwitch && location.pathname.toLowerCase() !== "/linustech" && !isBoca) return;
 
     let element = null;
     if(isFloatplane) element =
-        document.querySelector("div.av-player-control-wrapper > div.livestream-offline-container") ??
-        document.querySelector("._offlineContainer_1l3w1_1")
+        document.querySelector("._offlineContainer_1l3w1_1") ??
+        document.querySelector("div.av-player-control-wrapper > div.livestream-offline-container")
     ;
     if(isTwitch) element = document.querySelector("div.gMJXeQ:nth-child(1) > div:nth-child(1):not(.SugpE)");
 
     let chatElement = null
     if(isFloatplane) chatElement =
+        document.querySelector("div.chat-message-list:nth-child(1)") ??
         document.querySelector(".live-chat-panel-container > .live-chat-message-list-wrapper") ??
         document.querySelector(".live-chat-message-list-wrapper");
     if(isTwitch) chatElement = document.querySelector(".iWWhvN > div:nth-child(2) > div:nth-child(3)")
 
-    if(element != null) {
+    if(!isBoca && element != null) {
 
         let hasIframe = false;
         for (let child of element.children) {
@@ -91,7 +94,7 @@ async function replace() {
             chatIframe = document.createElement("iframe");
             chatIframe.style.backgroundColor = "transparent";
             chatIframe.allowTransparency = "true";
-            chatIframe.src = `https://whenplane.com/ltt-time?frame`;
+            chatIframe.src = `https://whenplane.com/ltt-time?frame` + (isBoca ? "&boca" : "");
             chatIframe.classList.add("whenplane_widget-ltttime");
             chatIframe.innerText = "Something went wrong when loading the Whenplane integration";
             chatIframe.className = "whenplane_widget-ltttime";
